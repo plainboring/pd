8fa0cd1cc75a84656d036d6f4c5dec0fddce3d2d
xinglong <xinglong940713@gmail.com>
2019-10-27 09:43:13 +0800

    remove unuse code

--- server/config/manager.go
+++ server/config/manager.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"github.com/BurntSushi/toml"
 	"github.com/pingcap/kvproto/pkg/configpb"
@@ -48,17 +47,6 @@ func NewConfigManager(client *clientv3.Client, rootPath string, member string) *
 	return cfg
 }
 
-//func (c *ConfigManager) InitConfigManager()  {
-//	c.mu.Lock()
-//	defer c.mu.Unlock()
-//	l,err := c.getTikvConfigList()
-//	if err != nil {
-//		panic(err)
-//	}
-//
-//	c.tikvEntryIndex = int32(len(l))
-//}
-//
 func (c *ConfigManager) NewTikvConfigReport(store_id uint64, config string)  {
 	raft_store := &cfgclient.Config{}
 	_,err := toml.Decode(config, raft_store)
@@ -138,62 +126,8 @@ func (c *ConfigManager) SaveTikvConfigIfNotExist(store_id uint64, config string)
 }
 
 func (c *ConfigManager) SaveTikvConfig(store_id uint64, config string) error {
-	//leaderPath := path.Join(c.rootPath, "leader")
-	//txn := kv.NewSlowLogTxn(c.client).If(append([]clientv3.Cmp{}, clientv3.Compare(clientv3.Value(leaderPath), "=", c.member))...)
-	//
-	//configPath := path.Join(c.rootPath, "tikv", strconv.FormatUint(store_id, 10))
-	//resp, err := txn.Then(clientv3.OpPut(configPath, config)).Commit()
-	//if err != nil {
-	//	return errors.WithStack(err)
-	//}
-	//
-	//if !resp.Succeeded {
-	//	return errors.New("save config failed, maybe we lost leader")
-	//}
 	configPath := path.Join("tikv", strconv.FormatUint(store_id, 10))
 	return c.baseKV.Save(configPath, config)
-	//return nil
-}
-
-func (c *ConfigManager) GetTikvEntries(store_id uint64) []*configpb.ConfigEntry {
-	entries,err := c.getTikvConfigList(store_id)
-	if err != nil || len(entries) == 0 {
-		return nil
-	}
-
-	var changed []*configpb.ConfigEntry
-	c.mu.Lock()
-	cfg,ok := c.tikvConfigs[store_id]
-	if !ok {
-		panic("there are no tikv store here")
-	}
-
-	if cfg.appliedIndex < len(entries) {
-		changed = entries[cfg.appliedIndex:]
-		c.applyChange(store_id, changed)
-		cfg.appliedIndex = len(entries)
-	}
-	c.mu.Unlock()
-
-	return changed
-}
-
-func (c *ConfigManager) GetTikvConfig(store_id uint64) (string, error) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-
-	cfg,ok := c.tikvConfigs[store_id]
-	if !ok {
-		return "",errors.New("there are no tikv in memory")
-	}
-
-	tikv_config := bytes.NewBuffer([]byte{})
-	err := toml.NewEncoder(tikv_config).Encode(cfg.config)
-	if err != nil {
-		return "",err
-	}
-
-	return tikv_config.String(),nil
 }
 
 func (c *ConfigManager) UpdatePDConfig(entry *configpb.ConfigEntry, cfg *Config) error {
@@ -356,19 +290,6 @@ func DecodeIntoConfigPDServer(entry *configpb.ConfigEntry, cfg *Config)  {
 			cfg.PDServerCfg.UseRegionStorage = bool_value
 		}
 	}
-}
-
-func (c *ConfigManager) UpdateTikvConfig(store_id uint64, entry *configpb.ConfigEntry) error {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-
-	configPath := path.Join(c.rootPath, "tikv")
-
-	if err := c.saveConfig(configPath, store_id, entry); err != nil {
-		return err
-	}
-
-	return nil
 }
 
 func (c *ConfigManager) DecodeTikvServerConfig(cfg *cfgclient.Config, entry *configpb.ConfigEntry)  {
@@ -703,49 +624,4 @@ func (c *ConfigManager) applyChange(store_id uint64, entry []*configpb.ConfigEnt
 	}
 
 	c.tikvConfigs[store_id] = cfg
-}
-
-func (c *ConfigManager) saveConfig(configPath string,store_id uint64, entry *configpb.ConfigEntry) error {
-	entries,err := c.getTikvConfigList(store_id)
-	if err != nil {
-		return err
-	}
-
-	entries = append(entries, entry)
-	data,err := json.Marshal(entries)
-	if err != nil {
-		return err
-	}
-
-	leaderPath := path.Join(c.rootPath, "leader")
-	txn := kv.NewSlowLogTxn(c.client).If(append([]clientv3.Cmp{}, clientv3.Compare(clientv3.Value(leaderPath), "=", c.member))...)
-
-	store_path := path.Join(configPath, strconv.FormatUint(store_id, 10))
-	resp, err := txn.Then(clientv3.OpPut(store_path, string(data))).Commit()
-	if err != nil {
-		return errors.WithStack(err)
-	}
-
-	if !resp.Succeeded {
-		return errors.New("save config failed, maybe we lost leader")
-	}
-
-	return nil
-	//return c.baseKV.Save(store_path, string(data))
-}
-
-func (c *ConfigManager) getTikvConfigList(store_id uint64) ([]*configpb.ConfigEntry,error) {
-	kvResp,err := c.baseKV.Load(path.Join("tikv", strconv.FormatUint(store_id, 10)))
-	if err != nil {
-		return nil,err
-	}
-	if kvResp == "" {
-		return nil,nil
-	}
-	entries := []*configpb.ConfigEntry{}
-	err = json.Unmarshal([]byte(kvResp), &entries)
-	if err != nil {
-		return nil,err
-	}
-	return entries,nil
 }
